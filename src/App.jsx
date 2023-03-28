093656c0eed68e0084b75146105c4bd5449909e6
albertollamass <alberto.ll.go@gmail.com>
2023-03-28 12:57:38 +0200

    [POK] Source code Pokedex

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import logo from './logo.svg'
+import { AppRouter } from './AppRouter'
+import { PokemonProvider } from './context/PokemonProvider';
+
+function App() {
+  return (
+    <PokemonProvider>
+      <AppRouter />
+    </PokemonProvider>
+    );
+}
+
+export default App
